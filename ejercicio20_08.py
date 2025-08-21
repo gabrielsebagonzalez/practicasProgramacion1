c8e69441a4cfc015f5f6ceb8d9736e2dd127302c
Gabriel Sebastian Gonzalez <gabrielsebastian@gmail.com>
2025-08-20 21:42:04 -0300

    se agrega ejercicio

---
+++ ejercicio20_08.py
@@ -0,0 +1,10 @@
+"""Una colonia de bacterias se duplica exponencialmente: al final de cada hora la cantidad total de bacterias se multiplica por 2.
+Pedir la cantidad inicial y la cantidad de horas, y calcular la cantidad de bacterias que abrá en total.
+Fórmula B = B_inicial * (2^horas)"""
+
+bacterias_inicial = int(input("Ingrese la cantidad inicial de bacterias: "))
+horas = int(input("Ingrese la cantidad de horas: "))
+
+total_bacterias = bacterias_inicial * (2 ** horas)
+
+print(f"La cantidad de total de bacterias después de {horas} horas son: {total_bacterias}")
